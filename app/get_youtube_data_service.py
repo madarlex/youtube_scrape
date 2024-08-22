7021d91a4d5315cdf641f936279b3055936d600a
madarlex <madarlex@gmail.com>
2024-08-22 19:27:16 +0700

    first commit

---
+++ app/get_youtube_data_service.py
@@ -0,0 +1,9 @@
+from app.youtube_services import YouTubeServices
+
+class GetYouTubeDataService(YouTubeServices):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_youtube_data_service(self):
+        pass
